4062b8b6bfcddd66470acd06d3a9c1eeb650c79d
LL020202 <1071812516@qq.com>
2024-09-08 20:25:46 +0800

    fist commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import cv2
+
+if __name__ == '__main__':
+    # Read the input image
+    img = cv2.imread('test.jpg')
+
+    # Convert into grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
